a754da6a9efc13473a839d0889fad0ab05ec0879
karan-2401 <karan342772@gmail.com>
2025-12-17 13:37:40 +0530

    changes

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
 const verifyToken = (req, res, next) => {
-  const token = req.cookies._vercel_jwt; // Get the token from cookies
+  const token = req.cookies.lmstoken; // Get the token from cookies
   if (!token) return res.status(401).send('Access denied. No token provided.');
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
